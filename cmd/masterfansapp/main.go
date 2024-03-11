63243c9084a77051ad773c76c6df63c77e05c68d
codelotus <95312169+codelotusr@users.noreply.github.com>
2024-03-12 00:01:50 +0200

    project restructured

--- cmd/masterfansapp/main.go
+++ cmd/masterfansapp/main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-    "net/http"
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
 )
 
 func main() {
-    r := chi.NewRouter()
-    r.Use(middleware.Logger)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-    // Respond to the root route
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Welcome to MasterFans!"))
-    })
+	// Respond to the root route
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to MasterFans!"))
+	})
 
-    // Sample API route
-    r.Route("/api", func(r chi.Router) {
-        r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
-            w.Write([]byte("Hello, MasterFans!"))
-        })
-    })
+	// Sample API route
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello, MasterFans!"))
+		})
+	})
 
-    http.ListenAndServe(":5000", r)
+	http.ListenAndServe(":5000", r)
 }
-
