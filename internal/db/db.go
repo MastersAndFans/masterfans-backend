e73d5c2bb5747f70a7d8165be2bfda41275a4702
codelotus <95312169+codelotusr@users.noreply.github.com>
2024-03-13 17:51:29 +0200

    cleaned up for docker-compose.yml

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,7 +14,8 @@ func InitDB() (*gorm.DB, error) {
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, dbname, port)
+	// configure ssl mode in production and set sslmode=require when deploying
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
