e1e8faf61caf06bf4aa1a055d2827c0f0e7f37ae
codelotus <95312169+codelotusr@users.noreply.github.com>
2024-03-14 01:38:49 +0200

    user entity created

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,1 +1,16 @@
 package models
+
+import "time"
+
+type User struct {
+	ID          uint      `gorm:"primaryKey"`
+	Email       string    `gorm:"unique;not null"`
+	Password    string    `gorm:"not null"`
+	Name        string    `gorm:"not null"`
+	Surname     string    `gorm:"not null"`
+	BirthDate   time.Time `gorm:"type:date;not null"`
+	PhoneNumber string
+	IsMaster    bool `gorm:"not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
