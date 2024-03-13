8a874b6627e9b7e58ff4decd0c48faac69825a3c
codelotus <95312169+codelotusr@users.noreply.github.com>
2024-03-14 01:47:14 +0200

    password hashing and checking utils

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -1,1 +1,16 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
