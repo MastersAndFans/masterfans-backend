6659c20cd681c1ed85a0937d3642cc83fcfdc477
Almond <almondCloud@outlook.com>
2024-03-18 20:24:50 +0200

    Add get user by id endpoint

--- cmd/masterfansapp/main.go
+++ cmd/masterfansapp/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/MastersAndFans/masterfans-backend/internal/db"
 	"github.com/MastersAndFans/masterfans-backend/internal/repository"
 	"github.com/MastersAndFans/masterfans-backend/pkg/auth"
 	"github.com/MastersAndFans/masterfans-backend/pkg/models"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"log"
-	"net/http"
+	"encoding/json"
+	"strconv"
 )
 
 func main() {
@@ -28,6 +33,42 @@ func main() {
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to MasterFans!"))
+	})
+
+	// get by id
+	r.Get("/api/user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id_string := chi.URLParam(r, "id")
+
+		// convert id to int64
+		id, err := strconv.ParseInt(id_string, 10, 64)
+		if err != nil {
+			http.Error(w, "ID must contain only digits", http.StatusBadRequest)
+			return
+		}
+
+		user, err := userRepo.FindById(context.Background(), id)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("User with ID %d not found", id), http.StatusBadRequest)
+			return
+		}
+
+		user_json, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK);
+		w.Write(user_json)
+	})
+
+	// list 
+	// TODO
+	r.Get("/api/user", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("list")
+		w.WriteHeader(http.StatusOK);
 	})
 
 	r.Post("/api/register", authHandler.RegisterHandler)
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -23,6 +23,15 @@ func (r *UserRepository) FindByEmail(ctx context.Context, email string) (*models
 	return &user, nil
 }
 
+func (r *UserRepository) FindById(ctx context.Context, id int64) (*models.User, error) {
+	var user models.User
+	err := r.db.WithContext(ctx).Where("id = ?", id).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func (r *UserRepository) CreateUser(ctx context.Context, user *models.User) error {
 	userRepo := NewRepository[models.User](r.db)
 
